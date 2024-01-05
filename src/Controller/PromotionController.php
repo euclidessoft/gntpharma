8b19fba1f9d09d44f0b288bd780135807074b4d2
modadg <modadg@gmail.com>
2024-01-05 13:06:32 +0000

    05-01-2023 13:06 page promotion

--- src/Controller/PromotionController.php
+++ src/Controller/PromotionController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/promotion")
+ * @Route("/{_locale}/Commande_Promotion")
  */
 class PromotionController extends AbstractController
 {
