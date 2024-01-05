4f71113357e0577e95dd2a1b2d2e758e8f4533c1
modadg <modadg@gmail.com>
2024-01-05 12:46:42 +0000

    05-01-2023 12:46 intgration design hajjalbayt

--- src/Entity/Promotion.php
+++ src/Entity/Promotion.php
@@ -22,6 +22,21 @@ class Promotion
      */
     private $designation;
 
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $debut;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $fin;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +50,42 @@ class Promotion
     public function setDesignation(string $designation): self
     {
         $this->designation = $designation;
+
+        return $this;
+    }
+
+    public function getDebut(): ?\DateTimeInterface
+    {
+        return $this->debut;
+    }
+
+    public function setDebut(\DateTimeInterface $debut): self
+    {
+        $this->debut = $debut;
+
+        return $this;
+    }
+
+    public function getFin(): ?\DateTimeInterface
+    {
+        return $this->fin;
+    }
+
+    public function setFin(\DateTimeInterface $fin): self
+    {
+        $this->fin = $fin;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
 
         return $this;
     }
