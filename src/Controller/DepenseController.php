06f1018dbdcd1f88191710064177b17c636f201f
modadg <modadg@gmail.com>
2024-12-24 11:45:05 +0000

    routes depenses

--- src/Controller/DepenseController.php
+++ src/Controller/DepenseController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/depense")
+ * @Route("/{_locale}/depense")
  */
 class DepenseController extends AbstractController
 {
