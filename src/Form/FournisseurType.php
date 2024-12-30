b5d307e1ea6e78874c3915e49dc9a9f9717705d2
Mamadou Bhoye Bah <bhoyemad11@gmail.com>
2024-12-30 15:55:21 +0000

    vue et crud fournisseur

---
+++ src/Form/FournisseurType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Fournisseur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FournisseurType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('designation')
+            ->add('adresse')
+            ->add('email')
+            ->add('pays')
+            ->add('telephone')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Fournisseur::class,
+        ]);
+    }
+}
