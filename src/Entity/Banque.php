7c6d5b49ff537f6b257886131561b4a9f39e119d
modadg <modadg@gmail.com>
2025-01-01 19:57:09 +0000

    ajust acaht

--- src/Entity/Banque.php
+++ src/Entity/Banque.php
@@ -6,6 +6,7 @@ use App\Repository\BanqueRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
@@ -31,6 +32,11 @@ class Banque
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message = "Renseignez le numéro")
+     * @Assert\Regex(
+     * pattern= "/[1-9]/",
+     * match = true,
+     * message = "Numéro incorrecte")
      */
     private $compte;
 
