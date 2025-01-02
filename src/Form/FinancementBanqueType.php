7cecaf5bed2b232cb90b6a0fa80b435aee2acd18
Mamadou Bhoye Bah <bhoyemad11@gmail.com>
2025-01-02 18:55:34 +0000

    financement vue

--- src/Form/FinancementBanqueType.php
+++ src/Form/FinancementBanqueType.php
@@ -10,7 +10,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class FinancementType extends AbstractType
+class FinancementBanqueType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
