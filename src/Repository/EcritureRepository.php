9b9d4991bf76a37eaef5efbd27e4fb243bb71616
modadg <modadg@gmail.com>
2025-01-07 20:04:21 +0000

    rapport

--- src/Repository/EcritureRepository.php
+++ src/Repository/EcritureRepository.php
@@ -19,39 +19,78 @@ class EcritureRepository extends ServiceEntityRepository
         parent::__construct($registry, Ecriture::class);
     }
 
-    // /**
-    //  * @return Ecriture[] Returns an array of Ecriture objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('e')
-            ->andWhere('e.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('e.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
 
-    /*
-    public function findOneBySomeField($value): ?Ecriture
-    {
-        return $this->createQueryBuilder('e')
-            ->andWhere('e.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
     public function ouverture()
     {
         $query = $this->createQueryBuilder('a')
             ->Where('a.date < :genre')
             ->setParameter('genre', date('Y-m-d'))
+            ->getQuery();
+        return $query->getResult();
+    }
+
+    public function ouverturecaisse()
+    {
+        $query = $this->createQueryBuilder('a')
+            ->Where('a.date < :genre')
+            ->setParameter('genre', date('Y-m-d'))
+            ->andWhere('a.type = :caisse')
+            ->setParameter('caisse', "Espece")
+            ->getQuery();
+        return $query->getResult();
+    }
+    public function brouyard()
+    {
+        $query = $this->createQueryBuilder('a')
+            ->Where('a.date = :genre')
+            ->setParameter('genre', date('Y-m-d'))
+            ->getQuery();
+        return $query->getResult();
+    }
+    public function brouyardcaisse()
+    {
+        $query = $this->createQueryBuilder('a')
+            ->Where('a.date = :genre')
+            ->setParameter('genre', date('Y-m-d'))
+            ->andWhere('a.type = :caisse')
+            ->setParameter('caisse', "Espece")
+            ->getQuery();
+        return $query->getResult();
+    }
+    public function brouyardbanque()
+    {
+        $query = $this->createQueryBuilder('a')
+            ->Where('a.date = :genre')
+            ->setParameter('genre', date('Y-m-d'))
+            ->andWhere('a.type = :banque')
+            ->setParameter('banque', "Banque")
+            ->getQuery();
+        return $query->getResult();
+    }
+    public function daybrouyard($date)
+    {
+        $query = $this->createQueryBuilder('a')
+            ->Where('a.date = :genre')
+            ->setParameter('genre', $date)
+            ->getQuery();
+        return $query->getResult();
+    }
+
+    public function ouvertureplace($datedebut)
+    {
+        $query = $this->createQueryBuilder('a')
+            ->Where('a.date < :genre')
+            ->setParameter('genre', $datedebut)
+            ->getQuery();
+        return $query->getResult();
+    }
+
+    public function plage($date, $date1)
+    {
+        $query = $this->createQueryBuilder('a')
+            ->Where('a.date BETWEEN :debut AND :fin')
+            ->setParameter('debut', $date)
+            ->setParameter('fin', $date1)
             ->getQuery();
         return $query->getResult();
     }
