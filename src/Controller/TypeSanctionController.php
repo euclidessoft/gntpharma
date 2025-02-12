cb44c2ee69c1932e5c548c65c8f17dccd768a00c
Mamadou Bhoye Bah <bhoyemad11@gmail.com>
2025-02-12 19:13:20 +0000

    correction

--- src/Controller/TypeSanctionController.php
+++ src/Controller/TypeSanctionController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/{_locale}/SanctionType/")
+ * @Route("/{_locale}/SanctionType")
  */
 class TypeSanctionController extends AbstractController
 {
